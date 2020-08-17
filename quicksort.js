e41d82b2f544d40cd65ca59001225f69dc63c681
Prince <princechauhan1992@hotmail.com>
2020-08-17 05:44:33 +0530

    added quick sort

---
+++ quicksort.js
@@ -0,0 +1,19 @@
+const quicksort = (arr) => {
+  const length = arr.length;
+  if (length < 2) {
+    return arr;
+  }
+  const pivot = arr[length - 1];
+  const leftArr = [];
+  const rightArr = [];
+  for (let i = 0; i < length - 1; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+  return [...quicksort(leftArr), pivot, ...quicksort(rightArr)];
+};
+
+console.log(quicksort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
